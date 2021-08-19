bd242d504858fd19f591f41122c3cf2aef1ce931
Cleomenes Bezerra Ferreira Junior <cleomenes2011@gmail.com>
2021-08-19 11:50:03 -0300

    Configurando Estrutura base do programa

---
+++ CadastroSeries/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+﻿namespace CadastroSeries.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set;  }
+    }
+}
---
+++ CadastroSeries/Enum/Genero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CadastroSeries.Enum
+{
+    public enum Genero
+    {
+       Acao = 1,
+       Aventure,
+       comedia,
+       Documentario,
+       Drama,
+       Espionagem = 6,
+       Faroeste,
+       Fantasia,
+       Ficcao,
+       Musical,
+       Romance,
+       Terror = 13
+    }
+}
